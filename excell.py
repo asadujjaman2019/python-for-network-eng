fc01ddbca43b09fcac1c62023c5c2a63c44518ba
shiplu <md.shiplu.1981@gmail.com>
2023-03-21 20:03:30 +0600

    hello excell

--- excell.py
+++ excell.py
@@ -3,6 +3,5 @@ import openpyxl
 wb = openpyxl.load_workbook('e:/ACI-Contracts.xlsx')
 ws = wb.active
 
-print('Total number of rows: '+str(ws.max_row)+'. And total number of columns: '+str(ws.max_column))
-
-print('The value in cell A1 is: '+ws['B1'].value)
+values = [ws.cell(row=1,column=i).value for i in range(1,ws.max_column+1)]
+print(values)
