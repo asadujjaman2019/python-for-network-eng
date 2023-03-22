6c9e08102e9aff5e55afe7f0aeecdd9b8bfbdf7f
shiplu <md.shiplu.1981@gmail.com>
2023-03-22 16:10:53 +0600

    hello excell

--- 1stcode.py
+++ 1stcode.py
@@ -1,14 +1,50 @@
-import json
-
-
-person_dict = {"name": "sohan",
-               "languages": ["English", "bd"],
-               "married": True,
-               "country": "bangladesh",
-               "age":13
-              }
-
-with open('person.json', 'w') as json_file:
-  json.dump(person_dict, json_file)
-
-
+{
+   "items": [
+       {
+           "orderID": "0000001211",
+           "orderInvoiceNo": "1234567",
+           "OrderBlocks": [
+               {
+                   "lineNo": [1,4,6,8,9,1,4],
+                   "ProductCode": "#31451"
+               },
+               {
+                   "lineNo": 2,
+                   "ProductCode": "#64311"
+               },
+               {
+                   "lineNo": 3,
+                   "ProductCode": "#85959"
+               }
+           ]
+       },
+       {
+           "orderID": "0000001212",
+           "orderInvoiceNo": "1234568",
+           "OrderBlocks": [
+               {
+                   "lineNo": 7,
+                   "ProductCode": "#86869"
+               },
+               {
+                   "lineNo": [6,7,4,8,4,2],
+                   "ProductCode": "#10384"
+               },
+               {
+                   "lineNo": 12,
+                   "ProductCode": "#00873"
+               }
+           ]
+       },
+       {
+           "orderID": "0000001213",
+           "orderInvoiceNo": "1234569",
+           "OrderBlocks": [
+               {
+                   "lineNo": 76,
+                   "ProductCode": "#22291"
+               }
+           ]
+       }
+   ]
+}
--- excell.py
+++ excell.py
@@ -3,5 +3,5 @@ import openpyxl
 wb = openpyxl.load_workbook('e:/ACI-Contracts.xlsx')
 ws = wb.active
 
-values = [ws.cell(row=1,column=i).value for i in range(1,ws.max_column+1)]
-print(values)
+data=[ws.cell(row=i,column=2).value for i in range(2,12)]
+print(data)
